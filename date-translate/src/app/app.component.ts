5968a91592e0db211978237eb9357332eb7598ac
ahmedalkurdi07 <ahmedalkurdi07@gmail.com>
2024-08-25 06:19:40 +0300

    V001

---
+++ date-translate/src/app/app.component.ts
@@ -0,0 +1,95 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import moment from 'moment-hijri';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  errorMessage: string = ''; 
+
+  private hijriDayName: string = '';
+
+  get HijriDayName(): string {
+    return this.hijriDayName;
+  }
+
+  private dateDay: string = '';
+  private dateMonth: string = '';
+  private dateYear: string = '';
+
+  public convertedDay: string = '';
+  public convertedMonth: string = '';
+  public convertedYear: string = '';
+
+  translate: boolean = false;
+
+  get DateDay(): string {
+    return this.dateDay;
+  }
+  set DateDay(day: string) {
+    this.dateDay = day;
+  }
+
+  get DateMonth(): string {
+    return this.dateMonth;
+  }
+  set DateMonth(month: string) {
+    this.dateMonth = month;
+  }
+
+  get DateYear(): string {
+    return this.dateYear;
+  }
+  set DateYear(year: string) {
+    this.dateYear = year;
+  }
+
+  toggleAndTranslateDate(): void {
+    this.translate = !this.translate;
+    if (!this.isValidDate(this.dateYear, this.dateMonth, this.dateDay)) {
+      this.errorMessage = 'التاريخ غير صالح. يرجى التحقق من المدخلات.';
+      this.translate = !this.translate;  
+      return;
+    }
+
+    this.errorMessage = '';
+    if (this.translate) {
+
+      const hijriDate = moment(`${this.dateYear}-${this.dateMonth}-${this.dateDay}`, 'YYYY-MM-DD').format('iYYYY-iM-iD');
+      const [year, month, day] = hijriDate.split('-').map(Number);
+
+      this.convertedYear = year.toString();
+      this.convertedMonth = month.toString();
+      this.convertedDay = day.toString();
+
+
+      moment.locale('ar');
+      const hijriDayOfWeek = moment(`${year}-${month}-${day}`, 'iYYYY-iM-iD').format('dddd');
+      const daysOfWeek: { [key: string]: string } = {
+        'Sunday': 'الأحد',
+        'Monday': 'الاثنين',
+        'Tuesday': 'الثلاثاء',
+        'Wednesday': 'الأربعاء',
+        'Thursday': 'الخميس',
+        'Friday': 'الجمعة',
+        'Saturday': 'السبت'
+      };
+
+      this.hijriDayName = daysOfWeek[hijriDayOfWeek] || 'غير معروف';
+    }
+    this.translate = !this.translate;
+    
+  }
+  isValidDate(year: string, month: string, day: string): boolean {
+    const date = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD', true);
+    return date.isValid();  
+  }
+  title = 'date-translate';
+}
